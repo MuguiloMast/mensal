72011cb3410c96be749c5cab4228cbe90f873059
MuguiloMast <95955236+MuguiloMast@users.noreply.github.com>
2024-10-31 10:00:07 -0300

    Add files via upload

---
+++ god.js
@@ -0,0 +1,40 @@
+function cumprimentar(){
+    prompt("falaaa Tî, coloque seu nome pf")
+    
+    
+}
+ 
+function AtivarModoEscuro(estado){
+    if (estado == true){
+ document.getElementById("corpinho").
+ style.background = '#000'
+ document.getElementById("corpo").
+ style.Color = '#3d5b67'
+    }
+    
+    else {
+        document.getElementById ("corpinho").
+ style.backgroundColor = '#3d5b67'
+        document.getElementById ("corpo").
+ style.backgroundColor = '#3d5b67'
+    }
+
+
+
+
+}
+
+let botao = document.getElementById("botao").addEventListener("click", ()=>{
+    alert("oiee")
+})
+
+/*
+   document.getElementById("corpo").style.backgroundColor =
+    "#a38f58"
+    document.getElementById("input").style.backgroundColor =
+    "#a38f58"
+    document.getElementById("form").style.backgroundColor =
+    "#a38f58"
+    document.getElementById("").style.backgroundColor =
+    "#a38f58"
+    */
